04b37e76f962c082a2307a5205a81c445c9921c9
Nattharika-jeksungnern <nattharika25kata@gmail.com>
2026-01-28 00:28:49 +0700

    เพิ่มข้อมูลของ redis

--- src/core/settings.py
+++ src/core/settings.py
@@ -41,6 +41,10 @@ class Settings:
         self.hostname = "test"
         self.maxThread = 2
 
+        self.redis_host = "10.20.20.108"
+        self.redis_port = 5678
+        self.redis_db = 1
+
         self.secure_store = EncryptedConfig("secret.dat")
 
         self.logger = setup_logger("Worker")

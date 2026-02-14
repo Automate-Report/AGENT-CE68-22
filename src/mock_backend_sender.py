73659888f3b99dae7923ea7af1708e44c78bab19
Nattharika-jeksungnern <nattharika25kata@gmail.com>
2026-02-14 18:10:22 +0700

    เพิ่มข้อมูลในการทดสอบ pen test

--- src/mock_backend_sender.py
+++ src/mock_backend_sender.py
@@ -10,10 +10,10 @@ def send_test_jobs():
     print(f"📡 Sending test jobs to {queue_name}...")
     
     test_jobs = [
-        {"job_id": 1, "target_url": "http://testphp.vulnweb.com/search.php", "attack_type": "xss"},
+        {"job_id": 1, "target_url": "http://testphp.vulnweb.com/search.php", "attack_type": "xss", "credential": {"user": "test", "password": "test"}},
         # {"id": 102, "target_url": "http://xss-game.appspot.com", "attack_type": "xss", "credentials": {"user": "admin"}},
         # {"id": 103, "target_url": "http://demo.testfire.net", "attack_type": "sql_injection", "credentials": None},
-        # {"id": 104, "target_url": "http://example.com", "attack_type": "xss", "credentials": None},
+        {"job_id": 2, "target_url": "http://testphp.vulnweb.com/search.php", "attack_type": "xss", "credential": None},
     ]
 
     for job in test_jobs:

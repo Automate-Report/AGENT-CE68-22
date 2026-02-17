9b083c81c562edde99ebbf55c45a772254a16727
Nattharika-jeksungnern <nattharika25kata@gmail.com>
2026-02-17 21:39:00 +0700

    ปรับ deduplicator ให้ดูที่ signature

--- src/scanner/deduplicator.py
+++ src/scanner/deduplicator.py
@@ -3,8 +3,14 @@ class Deduplicator:
     def __init__(self):
         self.seen_urls = set()
 
-    def is_seen(self, item: str) -> bool:
-        return item in self.seen_urls
+    def is_seen(self, method: str, url_path: str, params: dict) -> bool:
+        param_keys = ",".join(sorted(params.keys()))
+
+        signature = f"{method}|{url_path}|{param_keys}"
+        if signature in self.seen_signatures:
+            return True
+        self.seen_signatures.add(signature)
+        return False
 
     def add(self, item: str):
         self.seen_urls.add(item)

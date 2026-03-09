96c376b82511f481647c5deb9b2d9ae4e4f51da3
Nattharika-jeksungnern <nattharika25kata@gmail.com>
2026-03-09 20:15:49 +0700

    f

--- src/core/settings.py
+++ src/core/settings.py
@@ -35,8 +35,8 @@ class Settings:
         self.hostname = socket.gethostname()
         self.maxThread = 2
 
-        # self.redis_host = "10.20.20.108"
-        self.redis_host = "127.0.0.1"
+        self.redis_host = "10.20.20.108"
+        # self.redis_host = "127.0.0.1"
         self.redis_port = 5678
         self.redis_db = 1
 
